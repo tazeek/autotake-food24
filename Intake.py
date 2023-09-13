ccbd3aeff38b2ab438e51b1ed138f680875d9d91
tazeek <tazeek.rakib@gmail.com>
2023-09-13 08:50:35 +0800

    Add intake oop

--- Intake.py
+++ Intake.py
@@ -2,13 +2,16 @@
 
 class Intake:
 
-    def __init__(self, id):
+    def __init__(self):
 
-        self._id = id
         self.nutrients_info = {}
+        self.heifa_list = []
     
     def add_nutrient(self, nutrient_id, nutrient_info):
 
         self.meals_intake[nutrient_id] = nutrient_info
 
-        
+    def add_nutrient_heifa_list(self, heifa_list):
+
+        self.heifa_list = heifa_list
+
