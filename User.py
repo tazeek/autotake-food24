809b6d1a661b1992458704a5f4ecc4a2c977294b
tazeek <tazeek.rakib@gmail.com>
2023-09-18 00:04:54 +0800

    Pseudocode added

--- User.py
+++ User.py
@@ -10,3 +10,12 @@ class User:
     def add_survey(self, survey_id, survey):
 
         self._surveys[survey_id] = survey
+
+    def print_user_nutrition(self):
+
+        # Extract the surveys
+
+        # Go meal by meal in surveys
+
+        # Print out
+        ...
