ddc9357a7e05f82a240377f007d9ee9b4364c5ae
tazeek <tazeek.rakib@gmail.com>
2023-09-18 00:19:53 +0800

    Output code added

--- User.py
+++ User.py
@@ -11,12 +11,22 @@ class User:
 
         self._surveys[survey_id] = survey
 
+    def _meals_output(self, meal_obj):
+
+        heifa_codes = meal_obj.heifa_list
+        print(heifa_codes)
+        print("\n")
+        return None
+
     def print_user_nutrition(self):
 
-        for survey_obj in self._surveys.values():
+        for id, survey_obj in self._surveys.items():
             
             # Go meal by meal in surveys
+            print(f"Printing for Survey ID {id}\n")
             meals_obj_list = survey_obj.meals_intake.values()
 
-        # Print out
-        ...
+            # Print out
+            [self._meals_output(meal_obj) for meal_obj in meals_obj_list]
+        
+        return None
