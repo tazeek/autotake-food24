2f1598b3032d282be7040c6389cf61fed511e3e1
tazeek <tazeek.rakib@gmail.com>
2023-09-20 21:54:45 +0800

    Differentiate recipe

--- heifa_composition.py
+++ heifa_composition.py
@@ -6,16 +6,27 @@ class FoodComposition:
 
         self._heifa_code = info_dict['heifa_code']
         self._8_digit_code = info_dict['8_digit_code']
+
+        self._is_recipe = self._is_recipe(info_dict['food_group'])
         
         return None
+    
+    def _is_recipe(self, food_group):
+
+        if "Recipe" in food_group:
+
+            return True
+        
+        return False
     
     def print_full_details(self):
 
         print(
             f"Heifa Code: {self._heifa_code}\n",
-            f"8 Digit Code: {self._8_digit_code}\n"
+            f"8 Digit Code: {self._8_digit_code}\n",
+            f"Is a recipe: {self._is_recipe}\n"
         )
-        
+
         return None
 
 
