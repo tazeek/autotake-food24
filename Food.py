19b028d654fb346599cba0693ee08797a911d472
tazeek <tazeek.rakib@gmail.com>
2023-09-20 15:19:04 +0800

    Add food information

--- Food.py
+++ Food.py
@@ -2,6 +2,11 @@
 
 class Food:
 
-    def __init__():
+    def __init__(self, heifa_id):
+
+        self._heifa_id = heifa_id
+        self._components = {}
+    
+    def add_components():
 
         ...
