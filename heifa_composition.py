c958d337e3b57a2ecad17f79d5239ad7ea2d62c2
tazeek <tazeek.rakib@gmail.com>
2023-09-20 22:28:43 +0800

    Let's begin one last time

--- heifa_composition.py
+++ heifa_composition.py
@@ -5,7 +5,7 @@ class FoodComposition:
     def __init__(self, info_dict) -> None:
 
         self._heifa_code = info_dict['heifa_code']
-        self._8_digit_code = info_dict['8_digit_code']
+        self._8_digit_code = info_dict['eight_digit_code']
 
         self._is_recipe = self._is_recipe(info_dict['food_group'])
 
