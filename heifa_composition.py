b193a9cf57e739968dedfe1b7a83fac07b9d8b82
tazeek <tazeek.rakib@gmail.com>
2023-09-20 21:39:22 +0800

    Populate the food composition

--- heifa_composition.py
+++ heifa_composition.py
@@ -2,7 +2,10 @@
 
 class FoodComposition:
 
-    def __init__():
+    def __init__(self, info_dict):
+
+        self._heifa_code = info_dict['heifa_code']
+        self._8_digit_code = info_dict['8_digit_code']
         ...
 
 
