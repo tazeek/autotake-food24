8faad76dabdc78a204e70571e4deb4e900ee0df2
tazeek <tazeek.rakib@gmail.com>
2023-09-20 21:59:09 +0800

    Add in the serving size and measure

--- heifa_composition.py
+++ heifa_composition.py
@@ -8,6 +8,10 @@ class FoodComposition:
         self._8_digit_code = info_dict['8_digit_code']
 
         self._is_recipe = self._is_recipe(info_dict['food_group'])
+
+        # We will add them in regardless
+        self._serving_size = info_dict['serving_size']
+        self._serving_measure = info_dict['serving_measure']
         
         return None
     
@@ -25,6 +29,8 @@ class FoodComposition:
             f"Heifa Code: {self._heifa_code}\n",
             f"8 Digit Code: {self._8_digit_code}\n",
             f"Is a recipe: {self._is_recipe}\n"
+            f"Serving Size: {self._serving_size}\n",
+            f"Serving measure: {self._serving_measure}\n"
         )
 
         return None
