fd2ee0a4e4e11c1c5be9889221b5cd9e12a7ed53
tazeek <tazeek.rakib@gmail.com>
2023-09-28 03:55:56 +0800

    Remove inplace

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-def _rename_columns(name_replacer_dict: dict, df: pd.DataFrame) -> None:
-    return df.rename(columns = name_replacer_dict, inplace=True)
+def rename_columns(name_replacer_dict: dict, df: pd.DataFrame):
+    return df.rename(columns = name_replacer_dict)
