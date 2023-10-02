320e6d5f96437cd7007879ec1caa2d8c1fe4b20f
tazeek <tazeek.rakib@gmail.com>
2023-10-03 07:08:15 +0800

    Attributes added

--- daily_calculator.py
+++ daily_calculator.py
@@ -2,5 +2,23 @@
 
 class DailyCalculator:
 
-    def __init__(self):
-        ...
+    def __init__(self, ingredients_dict, recipe_dict):
+        
+        self._ingredients = ingredients_dict
+        self._recipes = recipe_dict
+
+        self._daily_servings = {}
+
+        return None
+
+    @property
+    def ingredients(self):
+        return self._ingredients
+    
+    @property
+    def recipes(self):
+        return self._recipes
+    
+    @property
+    def daily_servings(self):
+        return self._daily_servings
