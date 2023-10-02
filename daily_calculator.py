9f49718574f554545e7e8bb24c5f3c30baeccb60
tazeek <tazeek.rakib@gmail.com>
2023-10-03 07:24:05 +0800

    Getting ready to work

--- daily_calculator.py
+++ daily_calculator.py
@@ -23,6 +23,38 @@ class DailyCalculator:
     def daily_servings(self):
         return self._daily_servings
     
+    def _calculate_serving(self, meal):
+        
+        ingredients_dict = self.ingredients
+        recipes_dict = self.recipes
+
+        for heifa_id, ingredient_obj in meal.items():
+
+            # Just in case....
+            if heifa_id not in ingredients_dict:
+                print(f"\nHEIFA ID {heifa_id} not found")
+                continue
+
+            portion_size = ingredient_obj.portion_size
+            energy_with_fibre = ingredient_obj.energy_with_fibre
+
+            heifa_obj = ingredients_dict[heifa_id]
+            food_group = heifa_obj.food_group
+
+            if heifa_obj.is_recipe:
+                ...
+
+        return None
+    
+    def _find_servings(self, meals_list):
+
+        for meal in meals_list:
+
+            self._calculate_serving(meal)
+
+        return None
+
+    
     def calculate_daily_servings(self, meals_daily_list):
 
         # Store in this hierarchy: Date -> Food group -> Serving size
@@ -32,5 +64,8 @@ class DailyCalculator:
             self._daily_servings[date] = {}
 
             # Calculate the servings
-            ...
-        ...
+            self._find_servings(meals_list)
+        
+        print(self._daily_servings)
+
+        return None
